a79154094edfe0f6e5b95105fbb1ab1b0e4129e1
CodebyAliyah <mr.aliyah059@gmail.com>
2025-03-01 02:06:17 +0500

    git commit "solve the api issue & map function"

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,42 +1,39 @@
-import aiStartupLandingPage from "../../public/images/ai-startup-landing-page.png";
-import AboutImage from "../../public/images/aboutme.png";
-import AboutImageMobile from "../../public/images/aboutme (2).png";
 import aboutali from "../../public/images/aboutali.png";
+import AboutImageMobile from "../../public/images/aboutme (2).png";
 import Image from "next/image";
 import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 
 export const AboutSection = () => {
   return (
-    <div className="flex items-center justify-center w-full px-4 md:px-8 lg:px-16 -mt-20" id="about">
+    <div className=" flex items-start justify-center -mt-32" id="about">
       <ContainerScroll
         titleComponent={
-          <div className="flex flex-col items-center justify-center md:gap-3 text-center mt-6">
-            <h1 className="font-cursive text-3xl md:text-5xl">About Me</h1>
-            <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+          <div className="flex flex-col items-center justify-center md:gap-3 -mb-28 md:-mb-0">
+            <h1 className="font-cursive text-3xl md:text-5xl text-center mt-6">
+              About Me
+            </h1>
+            <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
               A glimpse into my world
             </p>
           </div>
         }
       >
-        {/* Responsive Images */}
-        <div className="w-full flex justify-center">
-          <Image
-            src={aboutali}
-            alt="hero"
-            height={720}
-            width={1400}
-            className="rounded-2xl object-cover w-full h-auto max-w-3xl hidden md:block"
-            draggable={false}
-          />
-          <Image
-            src={AboutImageMobile}
-            alt="hero"
-            height={720}
-            width={1600}
-            className="rounded-2xl object-cover w-full h-auto max-w-3xl md:hidden"
-            draggable={false}
-          />
-        </div>
+        <Image
+          src={aboutali}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top hidden md:block"
+          draggable={false}
+        />
+        <Image
+          src={AboutImageMobile}
+          alt="hero"
+          height={720}
+          width={1600}
+          className="mx-auto rounded-2xl object-cover h-full object-left md:hidden "
+          draggable={false}
+        />
       </ContainerScroll>
     </div>
   );
